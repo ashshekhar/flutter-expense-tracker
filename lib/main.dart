e3426346029b5984f2772cab67ba511af4111fec
Shashwat Shekhar <svs6603@psu.edu>
2021-01-14 19:27:57 +0530

    Add Card()

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,24 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Expense Tracker App"),
-      )
-      ),
+          appBar: AppBar(
+            title: Text("Expense Tracker App"),
+          ),
+          body: Column(children: <Widget>[
+            Card(
+              child: Container(
+                  width: double.infinity,
+                  color: Colors.red,
+                  child: Text("Chart")),
+            ),
+            Card(
+              child: Text("List of transactions"),
+            ),
+          ])),
     );
   }
 }
