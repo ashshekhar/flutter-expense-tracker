52d2452104c1d7c09df116fea149937042cb165d
Shashwat Shekhar <svs6603@psu.edu>
2021-01-18 00:55:16 +0530

    Minor UI improvements

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -27,7 +27,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSpent,
       };
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {
